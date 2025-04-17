c837b7b10239f562ab4c884fb381f0f5fba71707
Julian PR <j.pearbach@yahoo.com>
2025-04-17 14:27:30 -0700

    added random nudge to velocity and tweaked default parameters

--- flockforce/cpu-boids/boid_manager.gd
+++ flockforce/cpu-boids/boid_manager.gd
@@ -16,23 +16,13 @@ var updates_enabled : bool = false:
 		updates_enabled = new_val
 		PhysicsServer3D.set_active(new_val)
 
-@export 
-var spawn_radius: float = 50.0
-
-@export
-var random_velocity_strength: float = 1.0
-
-@export 
-var separation_strength: float = 1.0
-
-@export
-var alignment_strength: float = 1.0
-
-@export
-var cohesion_strenth: float = 1.0
-
-@export
-var neighborhood_size: float = 20.0
+@export var spawn_radius            : float = 50.0
+@export var random_velocity_strength: float = 1.0
+@export var separation_strength     : float = 1.0
+@export var alignment_strength      : float = 3.0
+@export var cohesion_strenth        : float = 1.2
+@export var random_nudge_strenth    : float = 0.05
+@export var neighborhood_size       : float = 15.0
 
 var boids: Array[RigidBody3D] = []
 
@@ -44,6 +34,9 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if updates_enabled:
 		update_boids_velocity(delta)
+
+func rand_direction() -> Vector3:
+	return  Vector3(rng.randfn(), rng.randfn(), rng.randfn()).normalized()	
 
 func initialize_boids():
 	for boid in boids:
@@ -66,11 +59,7 @@ func initialize_boids():
 			rng.randf_range(-spawn_radius, spawn_radius)
 		)
 		new_boid.position = spawn_location
-		new_boid.linear_velocity = Vector3(
-			rng.randfn(),
-			rng.randfn(),
-			rng.randfn()
-		).normalized() * random_velocity_strength
+		new_boid.linear_velocity = rand_direction() * random_velocity_strength
 		
 	
 
@@ -79,23 +68,22 @@ func update_boids_velocity(delta: float):
 	var separation = Vector3.ZERO
 	var alignment = Vector3.ZERO
 	var cohesion = Vector3.ZERO
+	var random_nudge = Vector3.ZERO
 	
 	for boid in boids:
 		var neighbors = get_boids_in_neighborhood(boid)
 		var CoM: Vector3 = get_center_of_mass(neighbors) # center of mass
 		var to_CoM = (CoM - boid.position).normalized() # possible bug : local vs global coords
 		var avg_direction: Vector3 = get_average_velocity(neighbors).normalized()
-		# TODO: set separation: vector pointing away from CoM
 		separation = -to_CoM
-		# TODO: set alignment: vector pointing towards average orientation
 		alignment = avg_direction
-		# TODO: set cohesion: vector pointing towards CoM
 		cohesion = to_CoM
-		# TODO: set the velocity to the weighted sum of these vectors
+		random_nudge = rand_direction() 
 		boid.linear_velocity = \
 			separation * separation_strength \
 			+ alignment * alignment_strength \
-			+ cohesion * cohesion_strenth
+			+ cohesion * cohesion_strenth \
+			+ random_nudge * random_nudge_strenth
 
 func get_boids_in_neighborhood(original: Node3D) -> Array[RigidBody3D]:
 	var neighbors: Array[RigidBody3D] = []
