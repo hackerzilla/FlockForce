85e6042c00dcc7208be17952c1663161a4d8a27e
Connor Armstrong <cparmstrong5@berkeley.edu>
2025-04-17 10:12:49 -0700

    compute shader draft written, no connection script yet and need to fix buffers

---
+++ flockforce/connor-compute/compute_boid.glsl
@@ -0,0 +1,71 @@
+#[compute]
+#version 450
+
+struct boid {
+    Vector
+}
+
+// Invocations in the (x, y, z) dimension
+layout(local_size_x = 2, local_size_y = 1, local_size_z = 1) in;
+
+// A binding to the buffer we create in our script
+layout(set = 0, binding = 0, std430) restrict buffer Position {
+    vec3 data[];
+}
+positions;
+
+layout(set = 0, binding = 1, std430) restrict buffer Velocity {
+    vec3 data[];
+}
+velocities;
+
+float neighborhood_size;
+
+float separation_strength = 1.0;
+float alignment_strength = 1.0;
+float cohesion_strength = 1.0;
+
+
+// The code we want to execute in each invocation
+void main() {
+    // gl_GlobalInvocationID.x uniquely identifies this invocation across all work groups
+    // my_data_buffer.data[gl_GlobalInvocationID.x] *= 2.0;
+
+    vec3 boid_position = positions.data[gl_GlobalInvocationID.x];
+    vec3 boid_velocity = velocities.data[gl_GlobalInvocationID.x];
+    vec3 avg_position = vec3();
+    vec3 avg_velocity = vec3();
+    int num_neighbors = 0;
+    int total_boids;
+
+    for (int i = 0; i < total_boids; i++) {
+        if (i == gl_GlobalInvocationID.x) {
+            continue;
+        }
+        vec3 other_position = positions.data[i];
+        vec3 other_velocity = velocities.data[i];
+        vec3 pos_difference = boid_position - other_position;
+        if (length(pos_difference) > neighborhood_size) {
+            continue;
+        }
+        avg_position += other_position;
+        avg_velocity += other_velocity;
+        num_neighbors += 1;
+    }
+
+    avg_position /= num_neighbors;
+    avg_velocity /= num_neighbors;
+    vec3 to_com = normalize(avg_position - boid_position);
+    vec3 separation = -to_com;
+    vec3 cohesion = to_com;
+    vec3 alignment = avg_direction;
+
+    boid_velocity = (separation * separation_strength) + (alignment * alignment_strength) + (cohesion * cohesion_strength);
+    boid_position = boid_position + boid_velocity; // should this include something related to timesteps?
+
+    positions.data[gl_GlobalInvocationID.x] = boid_position;
+    velocities.data[gl_GlobalInvocationID.x] = boid_velocity;
+
+
+
+}
