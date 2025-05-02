027afbddd106182562a4418cbce8b3a2113ec53d
Connor Armstrong <cparmstrong5@berkeley.edu>
2025-05-02 16:53:14 -0700

    two buffers working

--- flockforce/connor-compute/compute_boid.glsl
+++ flockforce/connor-compute/compute_boid.glsl
@@ -6,18 +6,30 @@
 layout(local_size_x = 2, local_size_y = 1, local_size_z = 1) in;
 
 // A binding to the buffer we create in our script
-layout(set = 0, binding = 0, std430) restrict buffer Position {
+layout(set = 0, binding = 0, std430) restrict buffer Position_a {
     vec3 data[];
 }
-positions;
+positions_a;
 
-layout(set = 0, binding = 1, std430) restrict buffer Velocity {
+layout(set = 0, binding = 1, std430) restrict buffer Velocity_a {
     vec3 data[];
 }
-velocities;
+velocities_a;
+layout(set = 0, binding = 2, std430) restrict buffer Position_b {
+    vec3 data[];
+}
+positions_b;
 
-layout(set = 0, binding = 2, std430) restrict buffer Params{
+layout(set = 0, binding = 3, std430) restrict buffer Velocity_b {
+    vec3 data[];
+}
+velocities_b;
+
+
+// This is the buffer for int params (boid count only)
+layout(set = 0, binding = 4, std430) restrict buffer Params{
     int boid_count;
+    int current_buffer;
 } params;
 
 
@@ -33,8 +45,16 @@ void main() {
     // gl_GlobalInvocationID.x uniquely identifies this invocation across all work groups
     // my_data_buffer.data[gl_GlobalInvocationID.x] *= 2.0;
 
-    vec3 boid_position = positions.data[gl_GlobalInvocationID.x];
-    vec3 boid_velocity = velocities.data[gl_GlobalInvocationID.x];
+    vec3 boid_read_position;
+    vec3 boid_read_velocity;
+    if (params.current_buffer == 0) {
+        boid_read_position = positions_a.data[gl_GlobalInvocationID.x];
+        boid_read_velocity = velocities_a.data[gl_GlobalInvocationID.x];
+    } else {
+        boid_read_position = positions_b.data[gl_GlobalInvocationID.x];
+        boid_read_velocity = velocities_b.data[gl_GlobalInvocationID.x];
+    }
+    
     vec3 avg_position = vec3(0.0);
     vec3 avg_velocity = vec3(0.0);
     int num_neighbors = 0;
@@ -44,9 +64,17 @@ void main() {
         if (i == gl_GlobalInvocationID.x) {
             continue;
         }
-        vec3 other_position = positions.data[i];
-        vec3 other_velocity = velocities.data[i];
-        vec3 pos_difference = boid_position - other_position;
+        vec3 other_position;
+        vec3 other_velocity;
+        if (params.current_buffer == 0) {
+            other_position = positions_a.data[i];
+            other_velocity = velocities_a.data[i];
+        } else {
+            other_position = positions_b.data[i];
+            other_velocity = velocities_b.data[i];
+        }
+
+        vec3 pos_difference = boid_read_position - other_position;
         if (length(pos_difference) > neighborhood_size) {
             continue;
         }
@@ -58,19 +86,25 @@ void main() {
         avg_position /= num_neighbors;
         avg_velocity /= num_neighbors;
     } else {
-        avg_position = boid_position;
-        avg_velocity = boid_velocity;
+        avg_position = boid_read_position;
+        avg_velocity = boid_read_velocity;
     }
-    vec3 to_com = normalize(avg_position - boid_position);
+    vec3 to_com = normalize(avg_position - boid_read_position);
     vec3 separation = -to_com;
     vec3 cohesion = to_com;
     vec3 alignment = avg_velocity;
 
-    boid_velocity = (separation * separation_strength) + (alignment * alignment_strength) + (cohesion * cohesion_strength);
-    boid_velocity = normalize(boid_velocity);
-    boid_position = boid_position + boid_velocity * 0.1; // should this include something related to timesteps?
+    boid_read_velocity = (separation * separation_strength) + (alignment * alignment_strength) + (cohesion * cohesion_strength);
+    boid_read_velocity = normalize(boid_read_velocity);
+    boid_read_position = boid_read_position + boid_read_velocity * 0.1; // should this include something related to timesteps?
 
-    positions.data[gl_GlobalInvocationID.x] = boid_position;
-    velocities.data[gl_GlobalInvocationID.x] = boid_velocity;
+    if (params.current_buffer == 0) {
+        positions_b.data[gl_GlobalInvocationID.x] = boid_read_position;
+        velocities_b.data[gl_GlobalInvocationID.x] = boid_read_velocity;
+    } else {
+        positions_a.data[gl_GlobalInvocationID.x] = boid_read_position;
+        velocities_a.data[gl_GlobalInvocationID.x] = boid_read_velocity;
+    }
+    
 
 }
